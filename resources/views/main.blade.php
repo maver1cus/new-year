5634765524a3541f1f8b374e6b84f4bafc30f830
Sergey Petrov <mavericusdev@gmail.com>
2022-09-10 11:08:36 +0300

    feat: настраивает сборку стилей и скриптов для сайта

---
+++ resources/views/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Новогодгий сайт</title>
+  @vite(['resources/styles/site/app.scss'])
+  @vite('resources/js/site/app.ts')
+
+
+</head>
+<body>
+</body>
+</html>
